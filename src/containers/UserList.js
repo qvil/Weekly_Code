160d3ffd6ed38a7029b8ef434d7cb535aa28b3be
tshyeon <qvil1127@gmail.com>
2017-01-31 10:38:41 +0900

    Add confirm button

--- src/containers/UserList.js
+++ src/containers/UserList.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 // Redux
 import { connect } from 'react-redux';
 // Semantic-UI
-import { Button, Card, Icon, Divider, Image } from 'semantic-ui-react';
+import { Button, Card, Modal } from 'semantic-ui-react';
 // Custom Components
 import { getRandomNumber } from '../utils';
 import CardUserSimple from '../components/users/CardUserSimple';
 
 const userItems = [
     {
-        // userImage: "http://semantic-ui.com/images/avatar/large/steve.jpg",
-        // userId: "aaa",
+        userImage: "http://semantic-ui.com/images/avatar/large/steve.jpg",
+        userId: "aaa",
         userCharacter: "mafia",
         userDescription: "I'm not mafia",
         userKillScore: "4",
@@ -30,17 +30,51 @@ const userItems = [
         userDescription: "I'm docter",
         userKillScore: "0",
     },
+    {
+        userImage: "http://semantic-ui.com/images/avatar/large/jenny.jpg",
+        userId: "ddd",
+        userCharacter: "docter",
+        userDescription: "I'm docter",
+        userKillScore: "0",
+    },
 ];
+
+const styles = {
+  button: {
+    marginTop: "20px",
+    marginBotton: "10px"
+  }
+};
 
 class UserList extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+          fluidButton: {
+            fluid: true,
+            loading: false
+          },
+          resultWindow: {
+            open: false,
+          }
+        }
+    }
+
+    onClick = () => {
+      this.setState({
+        fluidButton: {
+          fluid: true,
+          loading: true,
+        }
+      });
     }
 
     render() {
         const { score } = this.props;
 
         return(
+          <div>
             <Card.Group>
                 { userItems.map((v, i) => {
                     return (
@@ -54,6 +88,19 @@ class UserList extends Component {
                     );
                 })}
             </Card.Group>
+            <Button
+              primary
+              fluid={ this.state.fluidButton.fluid }
+              loading={ this.state.fluidButton.loading }
+              style={ styles.button }
+              onClick={ this.onClick }
+            >Confirm</Button>
+            <Modal
+              open={ this.state.resultWindow.open }
+              dimeer={ this.state.resultWindow.dimmer }
+            ></Modal>
+          </div>
+
         );
     }
 }
